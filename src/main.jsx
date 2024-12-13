0fe8ce71025f63e0c53910d40afd1b6c3eebd499
Avishek Mondal <avizaz040@gmail.com>
2024-12-14 00:38:20 +0530

    main jxs update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import MainPage from './components/MainPage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MainPage from "./components/MainPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainPage/>
-  </StrictMode>,
-)
+    <BrowserRouter basename="/WeatherApp">
+      <MainPage />
+    </BrowserRouter>
+  </StrictMode>
+);
